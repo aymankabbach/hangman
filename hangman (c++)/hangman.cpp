652906ff1b3848e78d9acf4717ce76f40f881387
ayman <kabbachayman95@gmail.com>
2022-11-14 18:34:55 +0100

    get random country from vector

--- hangman (c++)/hangman.cpp
+++ hangman (c++)/hangman.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <time.h>
 using namespace std;
 void Load_Data_From_File_to_Vector(vector <string>& vCountries)
 {
@@ -15,11 +16,19 @@ void Load_Data_From_File_to_Vector(vector <string>& vCountries)
         }
     }
 }
+string get_random_country_from_vector(vector <string>& vCountries)
+{
+    srand (time(NULL));
+    int random = rand() % vCountries.size();
+    string random_country = vCountries[random];
+    return random_country;
+}
 int main()
 {
     vector <string> vCountries;
     Load_Data_From_File_to_Vector(vCountries);
-    
+    string random_country=get_random_country_from_vector(vCountries);
+    cout<<random_country<<endl;
     system("pause");
     return 0;
 }
