1071fec8b8a68f3f403e6edde0108fe4d4ea8a69
ayman <kabbachayman95@gmail.com>
2022-11-14 18:15:23 +0100

    load data from file to vector

--- hangman (c++)/hangman.cpp
+++ hangman (c++)/hangman.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
+void Load_Data_From_File_to_Vector(vector <string>& vCountries)
+{
+    fstream file;
+    file.open("countries.txt", ios::in);
+    if (file.is_open())
+    {
+        string Line;
+        while (getline(file,Line))
+        {
+            vCountries.push_back(Line);
+        }
+    }
+}
 int main()
 {
+    vector <string> vCountries;
+    Load_Data_From_File_to_Vector(vCountries);
+    
     system("pause");
     return 0;
 }
