2211bbe69dc3403b0e71667dce3d44a063b7d649
ayman <kabbachayman95@gmail.com>
2022-11-17 20:25:46 +0100

    check winning & losing conditions

--- hangman (c++)/hangman.cpp
+++ hangman (c++)/hangman.cpp
@@ -41,16 +41,52 @@ char get_user_letter()
 }
 bool is_letter_exist(string random_country, char letter,vector <char>& vUserCountry)
 {
-    bool letter_existed=false;
+    bool mistake=true;
     for (int x=0; x<random_country.size();x++)
     {
         if (random_country[x]==letter)
         {
             vUserCountry[x]=letter;
-            letter_existed=true;
+            mistake=false;
         }
     }
-    return letter_existed;
+    return mistake;
+}
+bool check_lost(bool mistake,int &attempts, string random_country)
+{
+    if (mistake)
+    {
+        attempts=attempts-1;
+        if (attempts==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+bool check_win(string random_country,vector <char>& vUserCountry)
+{
+    bool win=true;
+    for (int x=0;x<random_country.size();x++)
+    {
+        if (random_country[x]!=vUserCountry[x])
+        {
+            win=false;
+            return win;
+        }
+    }
+    return win;
+}
+void printing_vUserCountry(vector <char>& vUserCountry)
+{
+    for (char letter : vUserCountry)
+    {
+        cout<<letter;
+    }
+    cout<<endl;
 }
 void start_game(string random_country,vector <char>& vUserCountry)
 {
@@ -58,10 +94,28 @@ void start_game(string random_country,vector <char>& vUserCountry)
     bool loser=false;
     int attempts=5;
     bool mistake=false;
+    cout<<"try to guess the country's name\nyou have 5 attempts"<<endl;
     while (winner==false && loser==false)
     {
+        printing_vUserCountry(vUserCountry);
         char letter=tolower(get_user_letter());
         mistake=is_letter_exist(random_country,letter,vUserCountry);
+        loser=check_lost(mistake,attempts,random_country);
+        winner=check_win(random_country,vUserCountry);
+        if (winner)
+        {
+            cout<<random_country<<"\n";
+            cout<<"YOU WIN"<<endl;
+        }
+        if (loser)
+        {
+            cout<<"you lost\n";
+            cout<<"the word was "<<random_country<<endl;
+        }
+        if (winner==false && loser==false)
+        {
+            cout<<"you still have "<<attempts<<" attempts"<<endl;
+        }
     }
 }
 int main()
