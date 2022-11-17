101323889dbc6505cb5576f00b8574e9a127c30b
ayman <kabbachayman95@gmail.com>
2022-11-17 15:11:08 +0100

    check if letter exists & update UserCountry vector

--- hangman (c++)/hangman.cpp
+++ hangman (c++)/hangman.cpp
@@ -39,17 +39,39 @@ char get_user_letter()
     cin>>letter;
     return letter;
 }
+bool is_letter_exist(string random_country, char letter,vector <char>& vUserCountry)
+{
+    bool letter_existed=false;
+    for (int x=0; x<random_country.size();x++)
+    {
+        if (random_country[x]==letter)
+        {
+            vUserCountry[x]=letter;
+            letter_existed=true;
+        }
+    }
+    return letter_existed;
+}
+void start_game(string random_country,vector <char>& vUserCountry)
+{
+    bool winner=false;
+    bool loser=false;
+    int attempts=5;
+    bool mistake=false;
+    while (winner==false && loser==false)
+    {
+        char letter=tolower(get_user_letter());
+        mistake=is_letter_exist(random_country,letter,vUserCountry);
+    }
+}
 int main()
 {
     vector <string> vCountries;
     vector <char> vUserCountry;
-    bool winner=false;
-    bool loser=false;
-    int attempts=5;
     Load_Data_From_File_to_Vector(vCountries);
     string random_country=get_random_country_from_vector(vCountries);
     determine_the_length_of_usercountry(vUserCountry,random_country);
-    char letter=tolower(get_user_letter());
+    start_game(random_country,vUserCountry);
     system("pause");
     return 0;
 }
